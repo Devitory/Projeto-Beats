eb517d5bafebb67ecfc6018210803ab7ef5397f2
Guilherme M. Miranda <alchemist.software@proton.me>
2023-11-27 01:53:24 +0000

    Added type definitions.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,21 @@
-async function fetch() {
-  const data = { message: 'It works' };
-
-  const body = JSON.stringify(data, null, 2);
-
-  const status = 200;
-
+/**
+ * @param {import('./index').Request} request
+ * @param {unknown} env
+ * @param {import('./index').ExecutionContext} context
+ */
+async function fetch(request, env, context) {
   const headers = new Headers();
-
   headers.set('content-type', 'application/json;charset=UTF-8')
 
+  const data = {
+    request,
+    env,
+    context
+  };
+
+  const body = JSON.stringify(data, null, 2);
+  const status = 200;
   return new Response(body, { status, headers });
 }
 
-export default { fetch }
+export default { fetch };
